656528da2aa027f3b190726d5748d3c07b6d3b46
LeMinhHung120204 <lmhung777@gmail.com>
2025-04-22 13:55:56 +0700

    finish sha256

---
+++ Rtl/software/sha/obj/default/source.d
@@ -0,0 +1,10 @@
+obj/default/source.o: source.c ../sha_bsp/system.h ../sha_bsp/linker.h \
+  ../sha_bsp//HAL/inc/io.h ../sha_bsp//HAL/inc/alt_types.h
+
+../sha_bsp/system.h:
+
+../sha_bsp/linker.h:
+
+../sha_bsp//HAL/inc/io.h:
+
+../sha_bsp//HAL/inc/alt_types.h:
